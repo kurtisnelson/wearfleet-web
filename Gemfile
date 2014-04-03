c2e0a29ac139f7a93923bb9ecb42bad503d81346
Kurt Nelson <kurtisnelson@gmail.com>
2014-04-03 13:59:44 -0400

    Initial rails setup

---
+++ Gemfile
@@ -0,0 +1,38 @@
+source 'https://rubygems.org'
+ruby '2.1.0'
+
+gem 'bootstrap-sass'
+gem 'coffee-rails'
+gem 'devise'
+gem 'draper'
+gem 'foreman'
+gem 'jquery-rails'
+gem 'omniauth-gplus'
+gem 'pg'
+gem 'rails', '>= 4.0.0'
+gem 'rails_12factor'
+gem 'sass-rails'
+gem 'slim-rails'
+gem 'simple_form'
+gem 'turbolinks'
+gem 'uglifier'
+gem 'unicorn'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :test, :development do
+  gem 'dotenv'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'forgery'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'capybara-webkit'
+  gem 'coveralls'
+  gem 'database_cleaner'
+  gem 'launchy'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WearfleetWeb::Application.config.secret_key_base = 'faaff1ac280255c9b8b5cbd998ce8249876700c0b08eaf826f3dfeb28de0edd632e10936dcaea1cdc5a95162adfb6b46e98c5f024e0fa040748a5ee6cd0d8944'
