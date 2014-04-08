f824bcb8a95509928dc2fc2e80a6f0b62cb0d8e2
Kurt Nelson <kurtisnelson@gmail.com>
2014-04-08 10:33:59 -0400

    Basic chat

--- app/assets/javascripts/watch/main.js
+++ app/assets/javascripts/watch/main.js
@@ -5,7 +5,7 @@ var mapOptions = {
 };
 var map, me, pusher;
 var nodes = {};
-
+var onlineCount = 0;
 $(document).ready( function () {
     pusher = new Pusher(pusher_key, { authEndpoint: '/users/pusher_auth' });
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
@@ -24,28 +24,41 @@ $(document).ready( function () {
     });
 
     fleetChannel.bind('pusher:subscription_succeeded', function(members) {
+            updateOnlineCount(members.count);
             members.each(function(member) {
                     addMember(member);
             });
     });
 
     fleetChannel.bind('pusher:member_added', function(member) {
-      addMember(member);
+            addMember(member);
+            updateOnlineCount(onlineCount + 1);
     });
+
+    fleetChannel.bind('pusher:member_removed', function(member) {
+            updateOnlineCount(onlineCount - 1);
+    });
+
+    $("#chat button").click(function() {
+            var msg = $("#chat input[name=message]").val();
+            console.log(msg);
+            fleetChannel.trigger('client-all', {'message': msg});
+    });
+
+    fleetChannel.bind('client-all', addMessage);
+    fleetChannel.bind('client-dispatch', addMessage);
+    fleetChannel.bind('dispatch', addMessage);
+    fleetChannel.bind('all', addMessage);
 
     //WS.subscribe("nodes:camera", function(channel, node, data) {
     //    console.log("new camera frame");
     //    document.getElementById('camera').setAttribute('src', 'data:image/jpg;base64,' + data);
     //});
-
-    //WS.subscribe("notifications", function(channel, node, data) {
-    //    console.log("MSG from " + node + ": "+data);
-    //});
-    //
-    $("button#all").click(function () {
-        WS.publish("notifications:all", $("input#message").val());
-    });
 });
+
+function addMessage(data) {
+    $("#chat #room").append("<li>" + data.message + "</li>");
+}
 
 function addMember(member) {
                 if(member.id.indexOf('-') != -1){
@@ -54,6 +67,11 @@ function addMember(member) {
                         nodes[device] = new Device(pusher, device, member);
                     }
                 }
+}
+
+function updateOnlineCount(c) {
+  onlineCount = c;
+  $("#chat .count").text(c);
 }
 
 function showPosition(position) {
