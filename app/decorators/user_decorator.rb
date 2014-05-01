d3158d0d1c59646f659f92592da4efe41c58a8ab
Kurt Nelson <kurtisnelson@gmail.com>
2014-05-01 13:55:42 -0400

    Include email in authentication code

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,6 +2,6 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def auth_qr_image
-    [authentication_token, ENV['API_URL']].join(';').to_qr_image(size: "200x200").html_safe
+    [email, authentication_token, ENV['API_URL']].join(';').to_qr_image(size: "200x200").html_safe
   end
 end
